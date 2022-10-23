4b3557d8b5b476636655fe89b92c31114bcb63da
Rahul Gupta <98771367+rahulgupta12032003@users.noreply.github.com>
2022-10-23 23:45:45 +0530

    Updated Server

--- client/src/Pages/Register.jsx
+++ client/src/Pages/Register.jsx
@@ -1,58 +1,56 @@
 import {
-    Flex,
-    Box,
-    FormControl,
-    FormLabel,
-    Input,
-    InputGroup,
-    InputRightElement,
-    Stack,
-    Button,
-    Heading,
-    Text,
-    useColorModeValue,
-    Link,
-  } from "@chakra-ui/react";
-  import { useState } from "react";
-  import axios from "axios"
-  import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
-  import { useNavigate } from "react-router-dom";
-  
-  const Register = () => {
-    const [showPassword, setShowPassword] = useState(false);
-    const [showCPassword, setShowCPassword] = useState(false);
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [confirmPassword, setConfirmPassword] = useState("");
+  Flex,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Stack,
+  Button,
+  Heading,
+  Text,
+  useColorModeValue,
+  Link,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import axios from "axios";
+import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
+import { useNavigate } from "react-router-dom";
 
-    const navigate = useNavigate();
-  
-    const handleRegister = () => {
-       if(password !== confirmPassword) 
-       {
-        alert("Password is not matched")
-       }
-       else{
-         const user = {
-          name : name,
-          email : email,
-          password : password
-         }
-         console.log(user);
-       }
+const Register = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [showCPassword, setShowCPassword] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleRegister = () => {
+    if (password !== confirmPassword) {
+      alert("Password is not matched");
+    } else {
+      const user = {
+        name: name,
+        email: email,
+        password: password,
+      };
+      console.log(user);
     }
-  
-  
-    return (
+  };
+
+  return (
+    <div>
       <Flex
         minH={"100vh"}
         align={"center"}
         justify={"center"}
         bg={useColorModeValue("gray.50", "gray.800")}
       >
-        <Stack spacing={8} mx={"auto"} maxW="lg" py={12} px={6} >
-          <Stack align={"center"} >
+        <Stack spacing={8} mx={"auto"} maxW="lg" py={12} px={6}>
+          <Stack align={"center"}>
             <Heading fontSize={"4xl"} textAlign={"center"}>
               SIGN UP
             </Heading>
@@ -62,23 +60,39 @@ import {
             bg={useColorModeValue("white", "gray.700")}
             boxShadow={"lg"}
             p={8}
-            border="2px" 
+            border="2px"
             borderColor="gray"
             w="lg"
           >
             <Stack spacing={4}>
               <FormControl id="firstName" isRequired>
                 <FormLabel>Name</FormLabel>
-                <Input type="text" value={name} onChange={(e) => setName(e.target.value)}  required />
+                <Input
+                  type="text"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  required
+                />
               </FormControl>
               <FormControl id="email" isRequired>
                 <FormLabel>Email address</FormLabel>
-                <Input type="email" value={email} onChange={(e) => setEmail(e.target.value)}  required />
+                <Input
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
               </FormControl>
               <FormControl id="password" isRequired>
                 <FormLabel>Password</FormLabel>
                 <InputGroup>
-                  <Input type={showPassword ? "text" : "password"}  value={password} onChange={(e) => setPassword(e.target.value)}  minLength="6" required />
+                  <Input
+                    type={showPassword ? "text" : "password"}
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    minLength="6"
+                    required
+                  />
                   <InputRightElement h={"full"}>
                     <Button
                       variant={"ghost"}
@@ -90,11 +104,17 @@ import {
                     </Button>
                   </InputRightElement>
                 </InputGroup>
-              </FormControl>  
+              </FormControl>
               <FormControl id="confirmPassword" isRequired>
-               <FormLabel>Confirm Password</FormLabel>  
+                <FormLabel>Confirm Password</FormLabel>
                 <InputGroup>
-                  <Input type={showCPassword ? "text" : "password"}  value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}  minLength="6" required />
+                  <Input
+                    type={showCPassword ? "text" : "password"}
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                    minLength="6"
+                    required
+                  />
                   <InputRightElement h={"full"}>
                     <Button
                       variant={"ghost"}
@@ -116,24 +136,25 @@ import {
                   _hover={{
                     bg: "blue.500",
                   }}
-  
                   onClick={handleRegister}
-  
                 >
                   SIGN UP
                 </Button>
               </Stack>
               <Stack pt={6}>
                 <Text align={"center"}>
-                  Already a user? <Link color={"blue.400"} onClick={() => navigate("/login")} >Login</Link>
+                  Already a user?{" "}
+                  <Link color={"blue.400"} onClick={() => navigate("/login")}>
+                    Login
+                  </Link>
                 </Text>
               </Stack>
             </Stack>
           </Box>
         </Stack>
       </Flex>
-    );
-  };
-  
-  export default Register;
-  
+    </div>
+  );
+};
+
+export default Register;
