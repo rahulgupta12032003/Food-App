410f532cf78b0c827d2999b66112180687ad06d9
Rahul Gupta <98771367+rahulgupta12032003@users.noreply.github.com>
2022-10-31 23:36:13 +0530

    updated

--- server/Routes/userRoute.js
+++ server/Routes/userRoute.js
@@ -87,23 +87,6 @@ router.post("/login" , async (req, res) =>
 })
 
 
-////_________________________ upload images _____________________________////
 
-// const upload = multer({
-//   storage: multer.diskStorage({
-//     destination: function (req, file, cb){
-//       cb(null, "uploads")
-//     },
-//     filename: function (req, file, cb) {
-//       cb(null, file.fieldname + "-" + Date.now() + ".jpg")
-//     }
-//   })
-// }).single("image")
-
-// router.post('/uploads', upload, (req, res) => {
-//   res.send("File Uploaded Successfully!")
-// });
-
-//// ___________________________________________________________________ ////
 
 module.exports = router;
